ca9e477a43ff117a31eaabadf5c8d43876f298b3
iwconfig <snelhingst@gmail.com>
2017-03-23 20:28:49 +0000

    Major release 2.0: Added configuration menu
    On first run (no key in localStorage) you get prompted with
    the configuration menu. You can then open the menu again by
    clicking on the 'Config' label button on the site page.
    
    Configuration menu css is loaded via @resource.
    
    Other minior changes and patches also come with this release.
    Among the few changes is the @run-at setting, which now is
    'document-start'. The script does some things at document start
    and then waits for the DOM to load completely so it can finish
    the setup. This is much more consistent with the main page load.

--- client/svtplej.user.js
+++ client/svtplej.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         svtplej
 // @namespace    https://github.com/iwconfig/svtplay-dl-server
-// @version      1.3.4
+// @version      2.0
 // @description  adds a button to imdb search and a button to download the video, connecting to a download server which is through websocket which is running svtplay-dl, and shows the process in a progress bar and command output
 // @author       iwconfig
 // @match        http://www.svtplay.se/*
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.min.js
 // @grant        GM_addStyle
 // @grant        GM_getResourceURL
-// @run-at       document-idle
+// @grant        GM_getResourceText
+// @run-at       document-start
+// @resource     config_css https://gist.githubusercontent.com/iwconfig/1d054c941f1391fa0fdec61278857302/raw/8479840b272cf56cda60a9331e500a338f8758ce/svtplej.config.css
 // @resource     imdb_img http://i.imgur.com/Ht8jxfu.png
 // @resource     dl_img http://i.imgur.com/rFwkUrn.png
 // @resource     progress http://i.imgur.com/9UjqJVg.gif
@@ -16,33 +19,172 @@
 
 /* global __svtplay:true */
 /* global GM_getResourceURL */
+/* global GM_getResourceText */
 /* global GM_addStyle */
+/* global GM_config */
 
 
-// Tested in chrome with Tampermonkey.
+/* Only tested in chrome with Tampermonkey but other managers *should* work just as well. */
 
-// Bare with me.
-// i'm not particularly good at javascript and this is quite messy, but it gets the job done.
-// Check out my github if you want to contribute! :)
+/* Bare with me.
+   i'm not particularly good at javascript and this is quite messy, but it gets the job done.
+   Check out my github if you want to contribute! :)
 
-// Before you begin, point the 'ws_url' variable to the one you have set up in svtplay-dl-server.
-// Default is a secure connection to localhost on port 5000 which should be alright if the server is run locally.
+   When you have installed the script and browsed to svtplay url you are prompted to set up your config.
 
-// Also change the variables in the beginning of run() to whatever suits you.
-// Further down you can also edit the format conditionally when dealing with season and episode for example.
+   Also, in this script, change the variables in the beginning of run() to whatever suits you.
+   Further down you can also edit the filename format conditionally, when dealing with season and episode for example.
 
-// Some stuff, like categories and genres for example, is in Swedish by default,
-// I assume most of you who are using this are speaking swedish but if not or if you prefer another language
-// then submit an issue about this and i'll fix english translations for you.
-// Ultimately this script is only temporary as i plan to make it a standalone extension,
-// which would make translations among other things a piece of cake. So for now I keep it simple.
+   Some stuff, like categories and genres for example, is in Swedish by default.
+   I assume most of you who are using this are speaking swedish but translations will be available
+   as soon as i have the time for this. So for now I keep it simple. */
 
+var css = GM_getResourceText('config_css');
 
-window.addEventListener ("load", LocalMain, false);
+function checkSettings() {
+
+    var frame = document.createElement('div');
+    document.body.appendChild(frame);
+    var title = document.createElement('h1');
+    title.className = 'config_header_title';
+    title.innerHTML = `
+svt<div class="teve">
+<div id="antenn">
+<svg height="60" width="40" viewBox="0 0 100 100"><polyline points="1.8125 21.4375 25.25 38.75 41.5 81.5 48 49 61 36 42 13.5 61 1" style="fill:none;stroke:rgba(101, 108, 121, 0.7);;stroke-width:4"></polyline></svg></div>
+<div class="teve knappar" id="ONknapp"></div>
+<div class="teve knappar" id="knapp1"></div>
+<div class="teve knappar" id="knapp2"></div>
+<div class="teve" id="stjaernkrig">plej</div>
+</div> settings`;
+
+    GM_config.init(
+        {
+            'id': 'svtplej',
+            'title': title,
+            'fields':
+            {
+                'ws_host':
+                {
+                    'section': 'Server',
+                    'label': 'WebSocket host',
+                    'type': 'text',
+                    'default': 'localhost',
+                    'labelPos': 'above',
+                    'title': 'Set the HOST which points to your server. Default: localhost',
+                },
+                'ws_port':
+                {
+                    'label': 'WebSocket port',
+                    'type': 'text',
+                    'default': '5000',
+                    'labelPos': 'above',
+                    'title': 'Set which PORT the server is using. Default: 5000'
+                },
+                'ws_secure':
+                {
+                    'label': 'Secure connection?',
+                    'type': 'checkbox',
+                    'default': false,
+                    'title': "Connect using a secure connection. i.e: 'ws://' or 'wss://'."
+                },
+                'path':
+                {
+                    'section': 'Path locations',
+                    'label': 'Save path',
+                    'type': 'text',
+                    'labelPos': 'above',
+                    'title': 'Specify where the server should put the downloaded videos.'
+                },
+                'tmpdir':
+                {
+                    'label': 'Temporary path',
+                    'type': 'text',
+                    'labelPos': 'above',
+                    'title': 'Specify temporary directory. Type "default" for /tmp/svtplay_downloads',
+                    'default': 'default'
+                },
+            },
+            'frame': frame,
+            'css': css,
+            'events':
+            {
+                'open': function() {
+                    frame.className += ' svtplej_center';
+                    frame.style = '';
+                    if (!window.localStorage.svtplej) {
+                        var h3 = document.createElement('h3');
+                        var span = document.createElement('h3');
+                        h3.className = 'config_header_h3';
+                        h3.innerHTML = 'First run!';
+                        span.className = 'config_header_span';
+                        span.innerHTML = 'Please set up your config below.';
+                        span.style.fontSize = '20px';
+                        frame.firstChild.firstChild.appendChild(h3);
+                        frame.firstChild.firstChild.appendChild(span);
+                    }
+                    var divs = frame.getElementsByClassName('config_var');
+                    var labels = frame.getElementsByTagName('label');
+                    for (var i = 0; labels.length > i; i++) {
+                        if (labels[i] !== divs[i].firstChild) {
+                            divs[i].insertBefore(labels[i], divs[i].firstChild);
+                        }
+                        divs[i].setAttribute('data-tooltip', divs[i].title);
+                        divs[i].removeAttribute('title');
+                    }
+                    //var secHeads = document.getElementsByClassName('section_header');
+                    //secHeads[0].setAttribute('data-tooltip', 'Obligatory stuff');
+                    //secHeads[1].setAttribute('data-tooltip', 'Obligatory stuff');
+                    var resetBtn = document.getElementById('svtplej_resetLink');
+                    resetBtn.setAttribute('data-tooltip', resetBtn.title);
+                    resetBtn.removeAttribute('title');
+                    document.getElementById('svtplej_resetLink').innerHTML = '⟲';
+                    frame.appendChild(frame.firstChild.lastChild, frame);
+                    frame.insertBefore(frame.firstChild.firstChild, frame.firstChild);
+
+                },
+                'save': function() {
+                    GM_config.close();
+                }
+            }
+        });
+        if (!GM_config.get('ws_host') || !GM_config.get('ws_port') || !GM_config.get('path')) {
+            GM_config.open();
+            if (!window.localStorage.svtplej) {
+                var btn = document.getElementById('svtplej_closeBtn');
+                btn.disabled = true;
+                btn.style.background = 'rgba(0, 0, 0, 0.30);';
+                btn.style.opacity = '0.6';
+                btn.style.cursor = 'not-allowed';
+            } else {
+                var storage = JSON.parse(window.localStorage.svtplej);
+                for (var k in storage) {
+                    if (storage[k] === '') {
+                        document.getElementById('svtplej_field_'+k).style.background = 'rgba(255, 0, 0, 0.3)';
+                    }
+                    if (storage[k] === false) {
+                        document.getElementById('svtplej_ws_secure_field_label').setAttribute('style', 'background: rgba(255, 255, 0, 0.5); padding: 0 27px 0 4px; margin-right: -23px;');
+                    }
+                }
+                var span = document.createElement('h3');
+                span.className = 'config_header_span';
+                span.innerHTML = 'Something is missing. Look for fields marked in red or yellow.';
+                span.style.fontSize = '20px';
+                span.style.margin = '50px 0 -35px';
+                frame.firstChild.firstChild.appendChild(span);
+            }
+        }
+}
+
+window.addEventListener ("DOMContentLoaded", LocalMain, false);
 
 function LocalMain () {
     'use strict';
-    var ws_url = 'ws://localhost:5000'; // Websocket URL
+    checkSettings();
+    var ws_proto = 'ws';
+    if (GM_config.get('ws_secure')) {
+        ws_proto += 's';
+    }
+    var ws_url = ws_proto + '://' + GM_config.get('ws_host') + ':' + GM_config.get('ws_port'); // Websocket URL
 
     var imdb = document.createElement('img');
     imdb.src = GM_getResourceURL('imdb_img');
@@ -64,7 +206,7 @@ function LocalMain () {
 
     function run(){
 
-        ////// Variables for svtplay-dl
+        /* Variables for svtplay-dl */
         var programTitle = __svtplay.videoTitlePage.titlePage.programTitle;
         var title = __svtplay.videoTitlePage.video.programTitle; // optional: add .replace(': ', ' - ')
         var info = __svtplay.videoTitlePage.video.title;
@@ -72,19 +214,19 @@ function LocalMain () {
         var episode = null;
         var format = '{t}';
         var cmd_options = '-M -f --force-subtitle';
-        var tmpdir = 'default'; // Default is '/tmp/svtplay_downloads/'
-        var path = '/media/SVT';
+        var tmpdir = GM_config.get('tmpdir'); // Default is '/tmp/svtplay_downloads/'
+        var path = GM_config.get('path');
 
         if (path.substr(-1) !== '/') {
             path += '/';
         }
 
-        var genres = [ 'Drama', 'Humor', 'Livsstil', 'Underhållning', 'Kultur', 'Samhälle & fakta', 'Nyheter', 'Sport', 'Barn', 'Komedi' ];
+        var genres = [ 'Drama', 'Humor', 'Livsstil', 'Underhållning', 'Kultur', 'Samhälle & fakta', 'Nyheter', 'Sport', 'Barn', 'Komedi', 'Animerat' ];
         var clusters = [];
         var access_service = __svtplay.videoTitlePage.video.accessService;
 
-        for (var i = 0; i < __svtplay.videoTitlePage.video.clusters.length; i++) {
-            clusters.push(__svtplay.videoTitlePage.video.clusters[i].name);
+        for (var i = 0; i < __svtplay.videoTitlePage.clusters.length; i++) {
+            clusters.push(__svtplay.videoTitlePage.clusters[i].name);
         }
 
         if (__svtplay.videoTitlePage.video.titleType === "CLIP") {
@@ -93,20 +235,23 @@ function LocalMain () {
             if (__svtplay.videoTitlePage.video.episodic && !clusters.includes('Dokumentär')) {
                 path += 'TV-serier/';
             } else {
-                var category = ['Dokumentär', 'Film', 'Kortfilm'];
-                for (var i = 0; i < clusters.length; i++) {
-                    if (category.includes(clusters[i])) {
-                        console.log(category[category.indexOf(clusters[i])]);
-                        path += category[category.indexOf(clusters[i])] + 'er/';
-                        if (__svtplay.videoTitlePage.video.episodic && clusters[i] === 'Dokumentär') {
-                            path += 'Dokumentärserier/';
+                if (clusters.includes('Kortfilm')) {
+                    path += 'Kortfilmer/';
+                } else {
+                    var category = ['Dokumentär', 'Film'];
+                    for (i = 0; i < clusters.length; i++) {
+                        if (category.includes(clusters[i])) {
+                            console.log(category[category.indexOf(clusters[i])]);
+                            path += category[category.indexOf(clusters[i])] + 'er/';
+                            if (__svtplay.videoTitlePage.video.episodic && clusters[i] === 'Dokumentär') {
+                                path += 'Dokumentärserier/';
+                            }
+                            break;
                         }
-                        break;
                     }
                 }
             }
         }
-
         if (access_service === 'signInterpretation') {
             path += 'Teckenspråkstolkat/';
         }
@@ -120,7 +265,7 @@ function LocalMain () {
                 season = __svtplay.videoTitlePage.video.season;
                 format += '/Season {s}';
             }
-            for (var i = 0; i < clusters.length; i++) {
+            for (i = 0; i < clusters.length; i++) {
                 if (genres.includes(clusters[i])) {
                     path += clusters[i] + '/';
                     console.log(path);
@@ -132,7 +277,7 @@ function LocalMain () {
                 path += programTitle + '/';
                 title = info;
             } else {
-                for (var i = 0; i < clusters.length; i++) {
+                for (i = 0; i < clusters.length; i++) {
                     if (genres.includes(clusters[i])) {
                         path += clusters[i] + '/';
                         console.log(path);
@@ -169,13 +314,23 @@ function LocalMain () {
                 }
             }
         }
-        ////// End of configuration
+        /* End of configuration */
 
         var span = document.createElement('span');
         span.setAttribute('class', 'play_video-page__title-element userscript');
         var span2 = document.createElement('span');
         span2.setAttribute('class', 'userscript');
         span2.innerHTML = '<br/>';
+
+        var configPopup = document.createElement('label');
+        configPopup.id = 'open-config';
+        configPopup.innerHTML = 'Config';
+        configPopup.style = 'color: #dee5bc; position: relative; display: block;';
+        configPopup.setAttribute('onclick', 'return false;');
+        span2.appendChild(configPopup);
+        configPopup.addEventListener('click', function() {
+            GM_config.open();
+        });
 
         if (__svtplay.videoTitlePage.video.titleType !== "CLIP") {
             var imdblink = document.createElement('a');
@@ -194,7 +349,6 @@ function LocalMain () {
         dl_link.setAttribute('onclick', 'return false;');
         dl_link.appendChild(dl_img);
         dl_link.addEventListener ("click", function () {
-
             if (document.getElementById('cancel')) {
                 if (document.getElementById('progress')) {
                     if (document.getElementById('finish') === null) {
@@ -263,6 +417,8 @@ function LocalMain () {
             });
 
             ws.onerror = function(error) {
+                progress.style.display = 'none';
+                configPopup.style.display = 'block';
                 dl_link.removeChild(progress_img);
                 dl_link.appendChild(dl_img);
                 dl_link.addEventListener("click", function (e) {
@@ -275,7 +431,6 @@ function LocalMain () {
                         LocalMain();
                     }
                 });
-
                 progresslabel.innerHTML = 'Is the websocket server running?';
 
             };
@@ -295,6 +450,7 @@ function LocalMain () {
                                 'articleId': articleId };
                 ws.send(JSON.stringify(jsondata));
 
+                configPopup.style.display = 'none';
                 var cancel = document.createElement('label');
                 cancel.id = 'cancel';
                 cancel.innerHTML = 'CANCEL';
@@ -307,7 +463,8 @@ function LocalMain () {
                     span2.removeChild(document.getElementById('delimiter'));
                     dl_link.removeChild(progress_img);
                     dl_link.appendChild(dl_img);
-
+                    configPopup.style.display = 'block'; // fix me: this stops working after the first click.
+                    //progress.style.display = 'none';
                 });
                 span2.appendChild(ShowHideOutput);
                 span2.insertAdjacentHTML('beforeend', '<span id="delimiter" style="color: #dee5bc"> | </span>');
@@ -369,7 +526,7 @@ function LocalMain () {
         document.getElementsByClassName('play_video-page__title')[0].appendChild(span);
         document.getElementsByClassName('play_video-page__title')[0].appendChild(span2);
         if (window.location.href.indexOf('/video/') === -1 && window.location.href.indexOf('/klipp/') === -1) {
-            span2.insertAdjacentHTML('afterend', '<br/>');
+            span2.insertAdjacentHTML('beforeend', '<br/>');
         }
     }
 
